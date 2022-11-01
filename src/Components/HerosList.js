d1a725e30b7ef7d1f60a6b4d75cc4572d8ea97bc
Josh Burd <Jburdt@gmail.com>
2022-11-01 17:32:33 -0400

    deleted search feature

--- src/Components/HerosList.js
+++ src/Components/HerosList.js
@@ -3,23 +3,8 @@ import HerosCard from './HerosCard';
 
 const HerosList = ({ heros }) => {
 
-  const [ search, setSearch ] = useState('');
-  
-
-  const handleSubmit = (e) => {
-    e.preventDefault()
-  };
-
-  const handleInput = (e) => {
-    setSearch(e.target.value)
-  };
-
   return (
-  <> 
-    <form onSubmit={handleSubmit}> 
-      <input onChange={handleInput} value={search} className='search' type='text' placeholder='Search hero...' ></input>
-      <button type='submit' className='submit'>Search</button>
-    </form>  
+  <>
         <main>
           <ul className='cardsholder'>
               {heros.map((hero) => {
